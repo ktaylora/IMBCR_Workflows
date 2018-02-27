bbe0748b3e021bdf61a7164cb1a27dd2bbc04dcd
ktaylora <kyle.a.taylor@gmail.com>
2018-02-27 11:28:24 -0700

    house keeping

--- iale_sohl_lulc_prediction_workflow.R
+++ iale_sohl_lulc_prediction_workflow.R
@@ -18,17 +18,17 @@ ggplot2_multivariate_densities <- function(densities=NULL, var=NULL, correction=
 
   rugs <- densities
   rugs[,2] <- rugs[,2] / correction
-  
+
   densities$year <- as.factor(densities$year)
   rugs$year <- as.factor(rugs$year)
-  
-  gg_plot <- ggplot(densities, aes_string(var, fill = 'year', col = 'year')) + geom_density(alpha=0.65) 
+
+  gg_plot <- ggplot(densities, aes_string(var, fill = 'year', col = 'year')) + geom_density(alpha=0.65)
   gg_plot <- gg_plot + scale_x_continuous(limits=xlim)
   gg_plot <- gg_plot + geom_rug(aes_string(var, color='year'), sides="b", size=0.25, alpha=0.8, data=rugs)
   gg_plot <- gg_plot + xlab(xlab) + ylab(ylab)
-  gg_plot <- gg_plot + scale_fill_brewer(type = 'seq', palette="YlGnBu") + scale_color_brewer(type = 'seq', palette="YlGnBu") 
-  
-  gg_plot + 
+  gg_plot <- gg_plot + scale_fill_brewer(type = 'seq', palette="YlGnBu") + scale_color_brewer(type = 'seq', palette="YlGnBu")
+
+  gg_plot +
     theme(
         axis.text = element_text(face="bold",size=10),
         text = element_text(face="bold",size=12),
@@ -48,76 +48,86 @@ ggplot2_multivariate_densities <- function(densities=NULL, var=NULL, correction=
 
 load(commandArgs(trailingOnly=T))
 
-r_gcam_45_rcp45_2014 <- raster::raster(
-    "/global_workspace/terry_sohl_sres_btu_30m_landcover_predictions/extracted/gplcc_gcam_45_rcp45_2014.tif"
-  )
-  
-r_gcam_45_rcp45_2050 <- raster::raster(
-    "/global_workspace/terry_sohl_sres_btu_30m_landcover_predictions/extracted/gplcc_gcam_45_rcp45_2050.tif"
+r_data_file <- tolower(paste(
+      tolower(argv[1]),
+      "_imbcr_hds_iale_prediction_workflow_",
+      gsub(format(Sys.time(), "%b %d %Y"), pattern = " ", replacement = "_"),
+      ".rdata",
+      sep = ""
+    ))
+
+r_gcam_45_2014 <- raster::raster(
+    "/global_workspace/terry_sohl_sres_btu_30m_landcover_predictions/extracted/gplcc_gcam_45_2014.tif"
   )
 
-r_gcam_45_rcp45_2100 <- raster::raster(
-    "/global_workspace/terry_sohl_sres_btu_30m_landcover_predictions/extracted/gplcc_gcam_45_rcp45_2100.tif"
+r_gcam_45_2050 <- raster::raster(
+    "/global_workspace/terry_sohl_sres_btu_30m_landcover_predictions/extracted/gplcc_gcam_45_2050.tif"
+  )
+
+r_gcam_45_2100 <- raster::raster(
+    "/global_workspace/terry_sohl_sres_btu_30m_landcover_predictions/extracted/gplcc_gcam_45_2100.tif"
   )
 
 s_2014 <- pts_to_landcover_metrics(
-       pts=s, 
-       grid_units=units, 
-       r=r_gcam_45_rcp45_2014, 
-       composition_statistics=composition_statistics
+       pts=s,
+       grid_units=units,
+       r=r_gcam_45_2014,
+       composition_statistics=composition_statistics,
+       force_valid_habitat_val=c(19,26) # only use 'grass' for our config. metric
      )
-     
+
 s_2050 <- pts_to_landcover_metrics(
-       pts=s, 
-       grid_units=units, 
-       r=r_gcam_45_rcp45_2050, 
-       composition_statistics=composition_statistics
+       pts=s,
+       grid_units=units,
+       r=r_gcam_45_2050,
+       composition_statistics=composition_statistics,
+       force_valid_habitat_val=c(19,26) # only use 'grass' for our config. metric
      )
 
 
 s_2100 <- pts_to_landcover_metrics(
-       pts=s, 
-       grid_units=units, 
-       r=r_gcam_45_rcp45_2100, 
-       composition_statistics=composition_statistics
+       pts=s,
+       grid_units=units,
+       r=r_gcam_45_2100,
+       composition_statistics=composition_statistics,
+       force_valid_habitat_val=c(19,26) # only use 'grass' for our config. metric
      )
 
 grass_densities <- rbind(
-    data.frame(year=2016, grass_ar=s_2014$grass_ar), 
+    data.frame(year=2016, grass_ar=s_2014$grass_ar),
     data.frame(year=2050, grass_ar=s_2050$grass_ar),
     data.frame(year=2100, grass_ar=s_2100$grass_ar)
   )
 
-dev.new()
-ggplot2_multivariate_densities(grass_densities, var='grass_ar', xlab="Grassland [Total Area] (km2)") 
+# dev.new()
+# ggplot2_multivariate_densities(grass_densities, var='grass_ar', xlab="Grassland [Total Area] (km2)")
 
 shrub_densities <- rbind(
-    data.frame(year=2016, shrub_ar=s_2014$shrub_ar), 
+    data.frame(year=2016, shrub_ar=s_2014$shrub_ar),
     data.frame(year=2050, shrub_ar=s_2050$shrub_ar),
     data.frame(year=2100, shrub_ar=s_2100$shrub_ar)
   )
 
-dev.new()
-ggplot2_multivariate_densities(shrub_densities, var='shrub_ar', xlab="Shrubland [Total Area] (km2)") 
+# dev.new()
+# ggplot2_multivariate_densities(shrub_densities, var='shrub_ar', xlab="Shrubland [Total Area] (km2)")
 
+# wetland_densities <- rbind(
+#     data.frame(year=2016, wetland_ar=s_2014$wetland_ar),
+#     data.frame(year=2050, wetland_ar=s_2050$wetland_ar),
+#     data.frame(year=2100, wetland_ar=s_2100$wetland_ar)
+#   )
 
-wetland_densities <- rbind(
-    data.frame(year=2016, wetland_ar=s_2014$wetland_ar), 
-    data.frame(year=2050, wetland_ar=s_2050$wetland_ar),
-    data.frame(year=2100, wetland_ar=s_2100$wetland_ar)
-  )
-
-dev.new()
-ggplot2_multivariate_densities(wetland_densities, var='wetland_ar', xlab="Wetland [Total Area] (km2)") 
+# dev.new()
+# ggplot2_multivariate_densities(wetland_densities, var='wetland_ar', xlab="Wetland [Total Area] (km2)")
 
 patch_densities <- rbind(
-    data.frame(year=2016, pat_ct=s_2014$pat_ct), 
+    data.frame(year=2016, pat_ct=s_2014$pat_ct),
     data.frame(year=2050, pat_ct=s_2050$pat_ct),
     data.frame(year=2100, pat_ct=s_2100$pat_ct)
   )
 
-dev.new()
-ggplot2_multivariate_densities(patch_densities, var='pat_ct', xlab="Patchiness [Patch Count] (km2)") 
+# dev.new()
+# ggplot2_multivariate_densities(patch_densities, var='pat_ct', xlab="Patchiness [Patch Count] (km2)")
 
 # Re-build our model selection criterion
 MOD_SEL_THRESHOLD <- max(which(model_selection_table@Full$delta < AIC_SUBSTANTIAL_THRESHOLD))
@@ -132,9 +142,8 @@ s_2014@data <- as.data.frame(
     scale(s_2014@data[,names(s_2014)], attr(m_scale, "scaled:center")[names(s_2014)], attr(m_scale, "scaled:scale")[names(s_2014)])
   )
 
-s_2014@data <- cbind(s_2014@data, s@data[,c('mat','map')])
-
-s_2014$effort <- effort     
+#s_2014@data <- cbind(s_2014@data, s@data[,c('mat','map')])
+s_2014$effort <- effort
 
 predicted_2014 <- par_unmarked_predict(
   unmarked_models=unmarked_models[MOD_SEL_THRESHOLD],
@@ -146,10 +155,9 @@ predicted_2014 <- par_unmarked_predict(
 s_2050@data <- as.data.frame(
     scale(s_2050@data[,names(s_2050)], attr(m_scale, "scaled:center")[names(s_2050)], attr(m_scale, "scaled:scale")[names(s_2050)])
   )
-  
-s_2050@data <- cbind(s_2050@data, s@data[,c('mat','map')]) 
 
-s_2050$effort <- effort 
+#s_2050@data <- cbind(s_2050@data, s@data[,c('mat','map')])
+s_2050$effort <- effort
 
 predicted_2050 <- par_unmarked_predict(
   unmarked_models=unmarked_models[MOD_SEL_THRESHOLD],
@@ -161,8 +169,9 @@ predicted_2050 <- par_unmarked_predict(
 s_2100@data <-  as.data.frame(
     scale(s_2100@data[,names(s_2100)], attr(m_scale, "scaled:center")[names(s_2100)], attr(m_scale, "scaled:scale")[names(s_2100)])
   )
-  
-s_2100@data <- cbind(s_2100@data, s@data[,c('mat','map')]) 
+
+#s_2100@data <- cbind(s_2100@data, s@data[,c('mat','map')])
+s_2100$effort <- effort
 
 predicted_2100 <- par_unmarked_predict(
   unmarked_models=unmarked_models[MOD_SEL_THRESHOLD],
@@ -171,6 +180,8 @@ predicted_2100 <- par_unmarked_predict(
   weights=aic_weights
 )
 
-s_2100$effort <- effort 
-
-
+save(
+    compress=T,
+    list=(ls(pattern="[a-z]")),
+    file=r_data_file
+  )
