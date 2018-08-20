4fc7806e86ac0f2624d8a458d0803240bd2496df
Kyle Taylor <kyle.a.taylor@gmail.com>
2018-08-20 13:55:35 -0600

    adding some habitat calculations for USDA-NASS

--- nm_audubon_ranch_analysis/nm_audubon_habitat_analyses.R
+++ nm_audubon_ranch_analysis/nm_audubon_habitat_analyses.R
@@ -3,7 +3,7 @@
 #
 
 ARGV = commandArgs(trailingOnly = T)
-MAXIMUM_DISTANCE_QUANTILE = 0.9 # censor observations that are way out in the shoulder
+MAXIMUM_DISTANCE_QUANTILE = 0.9 # censor observations out in the right-tail
 BIRD_CODE = ifelse(is.na(ARGV[1]), "WEME", toupper(ARGV[1]))
 
 #
